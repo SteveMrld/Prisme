5fb84704028e7139509dcc603501744269dd3b96
Steve <steve@confins.fr>
2026-04-05 08:23:46 +0000

    fix: Rétrospective ajoutée à la nav principale

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,6 +17,7 @@ const navItems = [
   { label: 'Culture', href: '/culture', className: 'culture' },
   { label: 'Portraits', href: '/portraits', className: 'portrait' },
   { label: 'Visuels', href: '/visuels', className: 'concept' },
+  { label: 'Rétrospective', href: '/retrospective', className: 'concept' },
 ]
 
 const secondaryItems = [
