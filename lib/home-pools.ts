50e83cdcb03f2e9623875f974a096f610e54968f
Steve Moradel <contact@soara.fr>
2026-08-06 13:39:29 +0000

    Hero: Majda Vincent a la place du verger et le fruit

--- lib/home-pools.ts
+++ lib/home-pools.ts
@@ -90,7 +90,7 @@ export const GF_SECONDARY_1_SLUG = 'terres-rares'
    sort, le total du hero restant fixé par HERO_TOTAL. */
 export const HERO_LEAD_SLUGS: string[] = [
   'trop-tot-pour-savoir-trop-tard-pour-agir',
-  'le-verger-et-le-fruit',
+  'sens-au-travail',
   'la-guerre-des-moi',
 ]
 
